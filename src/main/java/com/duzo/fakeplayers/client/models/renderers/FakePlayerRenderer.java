aedbe1852a2206e2963d61fbc98f284ba17b5f46
Duzo <james.hall01@outlook.com>
2023-03-01 23:56:58 +0000

    it worky

--- src/main/java/com/duzo/fakeplayers/client/models/renderers/FakePlayerRenderer.java
+++ src/main/java/com/duzo/fakeplayers/client/models/renderers/FakePlayerRenderer.java
@@ -14,7 +14,12 @@ import net.minecraft.client.renderer.entity.layers.CustomHeadLayer;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Map;
+import java.util.Objects;
 
 public class FakePlayerRenderer extends HumanoidEntityRenderer {
 
@@ -24,7 +29,8 @@ public class FakePlayerRenderer extends HumanoidEntityRenderer {
     @Override
     public ResourceLocation getTextureLocation(HumanoidEntity entity) {
         ResourceLocation texture;
-        texture = new ResourceLocation(FakePlayers.MODID, SkinGrabber.DEFAULT_DIR + entity.getName().getString().toLowerCase() + ".png");
+        texture = SkinGrabber.fileToLocation(new File(SkinGrabber.DEFAULT_DIR + entity.getName().getString().toLowerCase() + ".png"));
+        System.out.println(texture);
         return texture;
     }
 }
--- src/main/java/com/duzo/fakeplayers/util/SkinGrabber.java
+++ src/main/java/com/duzo/fakeplayers/util/SkinGrabber.java
@@ -1,18 +1,39 @@
 package com.duzo.fakeplayers.util;
 
+import com.mojang.blaze3d.platform.NativeImage;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class SkinGrabber {
     private static final String URL = "https://mineskin.eu/skin/";
-//    public static final String DEFAULT_DIR = "./fakeplayers/skins/";
+    public static final String DEFAULT_DIR = "/fakeplayers/skins/";
+
+    public static ResourceLocation fileToLocation(File file) {
+        NativeImage image = null;
+        try {
+            image = NativeImage.read(new FileInputStream(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return registerImage(image);
+    }
+
+    public static ResourceLocation registerImage(NativeImage image) {
+        TextureManager manager = Minecraft.getInstance().getTextureManager();
+        return manager.register("humanoid_", new DynamicTexture(image));
+    }
+
     public static void downloadSkinFromUsername(String username, File filepath) {
         try {
             URL url = new URL(URL + username);
