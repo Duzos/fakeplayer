0d2106d4cf94e792874b6e997bd6b38e708c7149
Duzo <james.hall01@outlook.com>
2023-05-24 23:45:29 +0100

    Added spawn egg items

--- platform-fabric-1.19.4/src/main/java/com/duzo/fakeplayers/Fakeplayers.java
+++ platform-fabric-1.19.4/src/main/java/com/duzo/fakeplayers/Fakeplayers.java
@@ -9,6 +9,8 @@ import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -28,6 +30,17 @@ public class Fakeplayers implements ModInitializer {
             FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, FakePlayerSlimEntity::new).dimensions(EntityDimensions.fixed(0.6f,1.8f)).build()
     );
 
+    // Items
+    public static final Item FAKE_PLAYER_SPAWN_EGG = Registry.register(
+            Registries.ITEM,
+            new Identifier(MOD_ID, "fake_player_spawn_egg"),
+            new SpawnEggItem(FAKE_PLAYER,0xe0280b,0x73eb44, new Item.Settings())
+    );
+    public static final Item FAKE_PLAYER_SLIM_SPAWN_EGG = Registry.register(
+            Registries.ITEM,
+            new Identifier(MOD_ID, "fake_player_slim_spawn_egg"),
+            new SpawnEggItem(FAKE_PLAYER_SLIM,0x73eb44,0xe0280b, new Item.Settings())
+    );
     /**
      * Runs the mod initializer.
      */
