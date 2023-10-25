ab52af1d3aac35860b79a27ec97304a3122b5252
Creativious <timothyu120@gmail.com>
2023-10-25 08:35:26 -0400

    architectury works now

---
+++ v1.20.2/common/src/main/java/com/duzo/fakeplayers/FakePlayers.java
@@ -0,0 +1,5 @@
+package com.duzo.fakeplayers;
+
+public class FakePlayers {
+    public static final String MOD_ID = "fakeplayers";
+}
---
+++ v1.20.2/fabric/src/main/java/com/duzo/fakeplayers/fabric/FakePlayersFabric.java
@@ -0,0 +1,10 @@
+package com.duzo.fakeplayers.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FakePlayersFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
