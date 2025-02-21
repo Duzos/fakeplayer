5436a713c6e35c10628bef6dd543c3aba9ab2ce9
James Hall <james.hall01@outlook.com>
2025-02-21 00:29:48 +0000

    chore: blank project

---
+++ common/src/main/java/dev/duzo/players/Constants.java
@@ -0,0 +1,11 @@
+package dev.duzo.players;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "players";
+	public static final String MOD_NAME = "Fake Players";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/dev/duzo/players/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package dev.duzo.players.platform;
+
+import dev.duzo.players.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
