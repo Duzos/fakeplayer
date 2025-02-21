183d5f15f3cc5243f5812229512a620fc4cd7b02
James Hall <james.hall01@outlook.com>
2025-02-21 05:24:26 +0000

    feat: interactions! feat: right-click with observer to toggle AI feat: stairs to toggle sitting feat: bed to toggle sleeping feat: slabs to toggle slim feat: paper to toggle nametag

--- common/src/main/java/dev/duzo/players/entities/FakePlayerEntity.java
+++ common/src/main/java/dev/duzo/players/entities/FakePlayerEntity.java
@@ -10,9 +10,13 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
@@ -22,11 +26,12 @@ import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.navigation.GroundPathNavigation;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 public class FakePlayerEntity extends PathfinderMob {
-	private static final EntityDataAccessor<Boolean> SITTING = SynchedEntityData.defineId(FakePlayerEntity.class, EntityDataSerializers.BOOLEAN);
+	private static final EntityDataAccessor<Integer> PHYSICAL_STATE = SynchedEntityData.defineId(FakePlayerEntity.class, EntityDataSerializers.INT);
 	private static final EntityDataAccessor<String> SKIN_KEY = SynchedEntityData.defineId(FakePlayerEntity.class, EntityDataSerializers.STRING);
 	private static final EntityDataAccessor<Boolean> SLIM = SynchedEntityData.defineId(FakePlayerEntity.class, EntityDataSerializers.BOOLEAN);
 
@@ -36,6 +41,15 @@ public class FakePlayerEntity extends PathfinderMob {
 
 	public FakePlayerEntity(Level level) {
 		this(FPEntities.FAKE_PLAYER.get(), level);
+	}
+
+	@Override
+	protected InteractionResult mobInteract(Player player, InteractionHand hand) {
+		if (hand == InteractionHand.MAIN_HAND && !player.level().isClientSide()) {
+			return InteractionRegistry.INSTANCE.get(player.getItemInHand(hand).getItem()).run((ServerPlayer) player, this);
+		}
+
+		return super.mobInteract(player, hand);
 	}
 
 	public static AttributeSupplier.Builder getHumanoidAttributes() {
@@ -74,7 +88,7 @@ public class FakePlayerEntity extends PathfinderMob {
 	public void readAdditionalSaveData(CompoundTag nbt) {
 		super.readAdditionalSaveData(nbt);
 
-		this.entityData.set(SITTING, nbt.getBoolean("sitting"));
+		this.entityData.set(PHYSICAL_STATE, nbt.getInt("State"));
 		this.entityData.set(SKIN_KEY, nbt.getString("SkinKey"));
 		this.entityData.set(SLIM, nbt.getBoolean("Slim"));
 	}
@@ -83,17 +97,44 @@ public class FakePlayerEntity extends PathfinderMob {
 	protected void defineSynchedData() {
 		super.defineSynchedData();
 
-		this.entityData.define(SITTING, false);
+		this.entityData.define(PHYSICAL_STATE, 0);
 		this.entityData.define(SKIN_KEY, "duzo");
 		this.entityData.define(SLIM, false);
 	}
 
-	public boolean isSitting() {
-		return this.entityData.get(SITTING);
+	public PhysicalState getPhysicalState() {
+		return PhysicalState.values()[this.entityData.get(PHYSICAL_STATE)];
 	}
 
-	public void setSitting(boolean val) {
-		this.entityData.set(SITTING, val);
+	public void setPhysicalState(PhysicalState state) {
+		this.entityData.set(PHYSICAL_STATE, state.ordinal());
+	}
+
+	public boolean isSitting() {
+		return this.getPhysicalState() == PhysicalState.SITTING;
+	}
+
+	public boolean isStanding() {
+		return this.getPhysicalState() == PhysicalState.STANDING;
+	}
+
+	@Override
+	public boolean hasPose(Pose pose) {
+		if (pose == Pose.SLEEPING) {
+			return this.getPhysicalState() == PhysicalState.LAYING;
+		}
+
+		return super.hasPose(pose);
+	}
+
+	@Override
+	public boolean isNoAi() {
+		return super.isNoAi() || this.getPhysicalState() == PhysicalState.LAYING;
+	}
+
+	@Override
+	public boolean isSleeping() {
+		return super.isSleeping() || this.getPhysicalState() == PhysicalState.LAYING;
 	}
 
 	protected String getStringKey() {
@@ -137,5 +178,11 @@ public class FakePlayerEntity extends PathfinderMob {
 
 	protected void setSlim(boolean val) {
 		this.entityData.set(SLIM, val);
+	}
+
+	public enum PhysicalState {
+		STANDING,
+		SITTING,
+		LAYING
 	}
 }
---
+++ common/src/main/java/dev/duzo/players/entities/InteractionRegistry.java
@@ -0,0 +1,100 @@
+package dev.duzo.players.entities;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class InteractionRegistry {
+	public static final InteractionRegistry INSTANCE = new InteractionRegistry();
+	private final HashMap<Item, Interaction> lookup = new HashMap<>();
+
+	private InteractionRegistry() {
+		this.defaults();
+	}
+
+	private static Set<Item> fromKey(TagKey<Item> key) {
+		Set<Item> result = new HashSet<>();
+
+		for (Item item : BuiltInRegistries.ITEM) {
+			if (item.getDefaultInstance().is(key)) {
+				result.add(item);
+			}
+		}
+
+		return result;
+	}
+
+	private void defaults() {
+		register(Items.OBSERVER, (player, entity) -> {
+			// toggle ai
+			entity.setNoAi(!entity.isNoAi());
+			player.sendSystemMessage(Component.literal(entity.isNoAi() ? "AI Disabled" : "AI Enabled"), true);
+			return InteractionResult.SUCCESS;
+		});
+
+		register(ItemTags.STAIRS, ((player, entity) -> {
+			// toggle sitting
+			entity.setPhysicalState(entity.isSitting() ? FakePlayerEntity.PhysicalState.STANDING : FakePlayerEntity.PhysicalState.SITTING);
+			player.sendSystemMessage(Component.literal(entity.isSitting() ? "Sitting" : "Standing"), true);
+
+			return InteractionResult.SUCCESS;
+		}));
+
+		register(ItemTags.BEDS, ((player, entity) -> {
+			// toggle sleeping
+			entity.setPhysicalState(entity.getPhysicalState() == FakePlayerEntity.PhysicalState.LAYING ? FakePlayerEntity.PhysicalState.STANDING : FakePlayerEntity.PhysicalState.LAYING);
+			player.sendSystemMessage(Component.literal(entity.getPhysicalState() == FakePlayerEntity.PhysicalState.LAYING ? "Laying" : "Standing"), true);
+
+			return InteractionResult.SUCCESS;
+		}));
+
+		register(ItemTags.SLABS, ((player, entity) -> {
+			// toggle slim
+			entity.setSlim(!entity.isSlim());
+			player.sendSystemMessage(Component.literal(entity.isSlim() ? "Slim" : "Normal"), true);
+
+			return InteractionResult.SUCCESS;
+		}));
+
+		register(Items.PAPER, ((player, entity) -> {
+			// toggle name visibility
+			entity.setCustomNameVisible(!entity.isCustomNameVisible());
+			player.sendSystemMessage(Component.literal(entity.isCustomNameVisible() ? "Name Visible" : "Name Hidden"), true);
+
+			return InteractionResult.SUCCESS;
+		}));
+
+
+	}
+
+	private Interaction fallback() {
+		return (player, entity) -> InteractionResult.PASS;
+	}
+
+	public void register(Item item, Interaction interaction) {
+		lookup.put(item, interaction);
+	}
+
+	public void register(TagKey<Item> tag, Interaction interaction) {
+		for (Item item : fromKey(tag)) {
+			register(item, interaction);
+		}
+	}
+
+	public Interaction get(Item item) {
+		return lookup.getOrDefault(item, fallback());
+	}
+
+	public interface Interaction {
+		InteractionResult run(ServerPlayer player, FakePlayerEntity entity);
+	}
+}
